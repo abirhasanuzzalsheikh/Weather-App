c9514957122da040b74d8c1b376282b3eac2d5ea
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-10-02 20:45:33 +0600

    display the current weather

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const searchInput = document.querySelector('.search-input');
+const currentWeatherDiv = document.querySelector('.current-weather');
 
 const API_KEY = '44753763a6be4204a1492957240210';
 
@@ -9,6 +10,11 @@ async function getWeatherDetails(cityName){
         const response = await fetch(API_URL);
         const data = await response.json();
 
+        const temperature = Math.floor(data.current.temp_c);
+        const description = data.current.condition.text;
+
+        currentWeatherDiv.querySelector('.temperature').innerHTML = `${temperature}<span>&deg;C</span>`;
+        currentWeatherDiv.querySelector('.descriptions').innerText = description;
         console.log(data);
     } catch(error){
         console.log(error);
