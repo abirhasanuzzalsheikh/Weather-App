b361e8a7021c041e10a4abaf5b7d2e264e6fc38a
AbirHasanUzzalSheikh <uzzal7341@gmail.com>
2024-10-02 21:09:02 +0600

    changing the weather icon

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const searchInput = document.querySelector('.search-input');
 const currentWeatherDiv = document.querySelector('.current-weather');
 
 const API_KEY = '44753763a6be4204a1492957240210';
+const weatherCode = {
+    clear:[1000],
+    clouds:[1003,1006,1009],
+    mist:[1030,1135,1147],
+    rain:[1063,1150,1168,1171,1180,1183,1198,1201,1240,1243,1246,1276],
+    moderate_heavy_rain:[1186,1189,1192,1195,1243,,1246],
+    snow:[1066,1069,1072,1114,1117,1204,1207,1210,1213,1216,1219,1222,1225,1237,1249,1252,1255,1258,1261,1264,1279,1282],
+    thunder:[1087,1279,1282],
+    thunder_rain:[1273,1276],
+}
 
 async function getWeatherDetails(cityName){
-    const API_URL = ` http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
+    const API_URL = ` http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
 
     try{
         const response = await fetch(API_URL);
@@ -12,8 +22,11 @@ async function getWeatherDetails(cityName){
 
         const temperature = Math.floor(data.current.temp_c);
         const description = data.current.condition.text;
+        const weatherIcon = Object.keys(weatherCode).find(icon=> weatherCode[icon].includes(data.current.condition.code));
+
 
         currentWeatherDiv.querySelector('.temperature').innerHTML = `${temperature}<span>&deg;C</span>`;
+        currentWeatherDiv.querySelector('.weather-icon').src = `iconImage/${weatherIcon}.svg`;
         currentWeatherDiv.querySelector('.descriptions').innerText = description;
         console.log(data);
     } catch(error){
